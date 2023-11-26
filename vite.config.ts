f226043d530452d580ad3c2691d4fec9f72c1c31
Bilel Barhoumi <infobilel@gmail.com>
2023-11-26 18:52:07 +0100

    Change vite config for build prod

--- vite.config.ts
+++ vite.config.ts
@@ -1,16 +1,20 @@
-import { fileURLToPath, URL } from 'node:url'
+import {fileURLToPath, URL} from 'node:url'
 
-import { defineConfig } from 'vite'
+import {defineConfig} from 'vite'
 import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [
-    vue(),
-  ],
-  resolve: {
-    alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
+export default defineConfig(({command, mode}) => {
+    console.log(mode)
+    return {
+        base: mode === 'production' ? '/vuejs-todos/' : '',
+        plugins: [
+            vue(),
+        ],
+        resolve: {
+            alias: {
+                '@': fileURLToPath(new URL('./src', import.meta.url))
+            }
+        }
     }
-  }
 })
